f4e45f179b234090f270f4754be315e732ed0bdc
ruderude <rude1979@gmail.com>
2023-03-21 11:43:30 +0900

    ゆう間に聞くためにエラーでプッシュ

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -6,6 +6,13 @@ import { client } from "../../libs/client"
 import SideBar from '../../components/layout/SideBar'
 import styles from './Contact.module.scss'
 import emailjs from '@emailjs/browser'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface Inputs {
+  to_name: string
+  to_email: string
+  message: string
+}
 
 const Contact: NextPage<any> = ({ categories, tags }) => {
   const title = `バーコード・ブログ: お問い合わせ`
@@ -15,20 +22,27 @@ const Contact: NextPage<any> = ({ categories, tags }) => {
   const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? ''
   const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ?? ''
 
-  // const [toName, setToName] = useState('')
-  // const [toEmail, setToEmail] = useState('')
-  // const [message, setMessage] = useState('')
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const [mail, setMail] = useState({
+  const [formData, setFormData] = useState({
     toName: '',
     toEmail: '',
     message: '',
   })
 
-  const { toName, toEmail, message } = mail
+  const { toName, toEmail, message } = formData
+  console.log("watch:", watch("to_name"));
 
-  const sendEmail = (e: any) => {
-    e.preventDefault()
+  const sendForm: SubmitHandler<Inputs> = (data) => {
+    console.log('sendForm')
+    console.log("onSubmit:", data);
+    return
+    // e.preventDefault()
 
     const params = {
       to_name: toName,
@@ -72,7 +86,7 @@ const Contact: NextPage<any> = ({ categories, tags }) => {
           </h2>
 
           <div className={styles.contact}>
-            <form onSubmit={sendEmail}>
+            <form onSubmit={handleSubmit(sendForm)}>
               <table className={styles.form_table}>
                 <tbody>
                   <tr>
@@ -80,8 +94,40 @@ const Contact: NextPage<any> = ({ categories, tags }) => {
                       <label htmlFor="to_name">氏名&nbsp;<span className={styles.badge}>必須</span></label>
                     </td>
                     <td className={styles.form_body}>
-                      <input type="text" name="to_name" id="to_name" placeholder="山田 太郎" />
-                      <div className={styles.error} id="to_name_error">氏名を入力してください</div>
+                      <input
+                        type="text"
+                        placeholder="山田 太郎"
+                        {...register("to_name", {
+                          required: true,
+                          pattern: /^[A-Za-z]+$/i,
+                          maxLength: 20,
+                        })}
+                      />
+                      {/* {errors.to_name?.type === "required" && (
+                        <span>This field is required</span>
+                      )}
+                      {errors.to_name?.type === "pattern" && errors.to_name?.types?.includes("pattern") && (
+                        <span>Name must contain alphabets only</span>
+                      )}
+                      {errors.to_name?.type === "maxLength" && errors.to_name?.types?.includes("maxLength") && (
+                        <span>Name must be less than or equal to 20 characters</span>
+                      )} */}
+
+                      {errors.to_name?.type === 'required' && (
+                        <div className={styles.error}>必須項目です。</div>
+                      )}
+                      {errors.to_name?.type === 'maxLength' && (
+                        <div className={styles.error}>20文字以下で入力してください。</div>
+                      )}
+                      {errors.to_name?.type === 'pattern' && (
+                        <div className={styles.error}>アルファベットのみ入力してください。</div>
+                      )}
+
+                      {/* <ul>
+                        {errors.to_name && Object.keys(errors.to_name).map(key => (
+                          <li key={key}>{errors.to_name[key].message}</li>
+                        ))}
+                      </ul> */}
                     </td>
                   </tr>
 
